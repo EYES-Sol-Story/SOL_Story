87120fd6cf74e1c11b109650d519d47718e9ae31
이지희 <som2plestory@naver.com>
2024-08-16 06:56:14 +0900

    Update UserChallenge.java

--- backend/src/main/java/com/eyes/solstory/domain/challenge/entity/UserChallenge.java
+++ backend/src/main/java/com/eyes/solstory/domain/challenge/entity/UserChallenge.java
@@ -19,19 +19,23 @@ import jakarta.persistence.TemporalType;
 @Table(name = "user_challenges")
 public class UserChallenge {
 
+	// 사용자 도전과제 기록 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_challenge_no")
     private Long userChallengeNo;
 
+    // 할당된 도전과제 객체
     @ManyToOne
     @JoinColumn(name = "challenge_no", nullable = false)
     private Challenge challenge;
 
+    // 도전과제 객체
     @ManyToOne
     @JoinColumn(name = "user_no", nullable = false)
     private User user;
 
+    // 도전과제 완료일자
     @Temporal(TemporalType.DATE)
     @Column(name = "completion_date")
     private Date completionDate;
