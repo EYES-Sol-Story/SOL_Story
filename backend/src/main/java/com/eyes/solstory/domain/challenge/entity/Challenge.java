94ea37db831958bc6ca4878c947965b9ef022d81
이지희 <som2plestory@naver.com>
2024-08-16 06:56:06 +0900

    Update Challenge.java

--- backend/src/main/java/com/eyes/solstory/domain/challenge/entity/Challenge.java
+++ backend/src/main/java/com/eyes/solstory/domain/challenge/entity/Challenge.java
@@ -11,26 +11,41 @@ import jakarta.persistence.Table;
 @Table(name = "challenges")
 public class Challenge {
 
+	// 도전과제 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "challenge_no")
     private Long challengeNo;
 
+    // 도전과제 이름
     @Column(name = "challenge_name", nullable = false, length = 255)
     private String challengeName;
 
+    // 도전과제 설명
     @Column(name = "challenge_description", length = 255)
     private String challengeDescription;
 
+    // 도전과제 완료 시 획득할 포인트(1000point = 1key)
     @Column(name = "reward_points", nullable = false)
     private Integer rewardPoints;
 
-    @Column(name = "reward_eco_mileage", nullable = false)
-    private Integer rewardEcoMileage;
-
+    // 도전과제 유형(0:일반 미션, 1: ECO 미션)
     @Column(name = "challenge_type", nullable = false)
     private Integer challengeType;
+    
+    // 기본 생성자
+    public Challenge() {}
 
     // Getters and Setters
+    
+    public Long getChallengeNo() {
+        return challengeNo;
+    }
+
+    public Integer getRewardPoints() {
+        return rewardPoints;
+    } 
+    
+    
     
 }
