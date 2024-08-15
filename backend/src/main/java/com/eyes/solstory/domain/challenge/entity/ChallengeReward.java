34369a86ef3b8d38470fbbe084c715986780fa28
이지희 <som2plestory@naver.com>
2024-08-16 06:56:11 +0900

    Update ChallengeReward.java

--- backend/src/main/java/com/eyes/solstory/domain/challenge/entity/ChallengeReward.java
+++ backend/src/main/java/com/eyes/solstory/domain/challenge/entity/ChallengeReward.java
@@ -1,5 +1,7 @@
 package com.eyes.solstory.domain.challenge.entity;
 
+import com.eyes.solstory.domain.user.entity.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,20 +15,41 @@ import jakarta.persistence.Table;
 @Table(name = "challenge_rewards")
 public class ChallengeReward {
 
+	// 리워드 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reward_no")
     private Long rewardNo;
 
+    // 사용자 객체
+    @ManyToOne
+    @JoinColumn(name = "user_no", nullable = false)
+    private User user;
+
+    // 도전과제 객체
     @ManyToOne
     @JoinColumn(name = "challenge_no", nullable = false)
     private Challenge challenge;
 
-    @Column(name = "points", nullable = false)
-    private Integer points;
+    // 도전과제 완료시 획득한 포인트
+    @Column(name = "reward_points", nullable = false)
+    private Integer rewardPoints;
 
-    @Column(name = "eco_mileage", nullable = false)
-    private Integer ecoMileage;
+    // 사용자가 획득한 총 열쇠 수
+    @Column(name = "total_keys", nullable = false)
+    private Integer totalKeys;
 
+    // 열쇠로 변환되지 않은 포인트(1000 포인트 미만)
+    @Column(name = "remaining_points", nullable = false)
+    private Integer remainingPoints;
+    
     // Getters and Setters
+    
+    public Long getChallengeNo() {
+        return challenge.getChallengeNo();
+    }
+
+    public Integer getRewardPoints() {
+        return challenge.getRewardPoints();
+    }
 }
