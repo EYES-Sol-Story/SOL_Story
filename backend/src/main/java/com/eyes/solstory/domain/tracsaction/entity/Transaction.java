28331dae7e32b919a0ac259f23bd64e623403398
이지희 <som2plestory@naver.com>
2024-08-16 06:56:49 +0900

    Update Transaction.java

--- backend/src/main/java/com/eyes/solstory/domain/tracsaction/entity/Transaction.java
+++ backend/src/main/java/com/eyes/solstory/domain/tracsaction/entity/Transaction.java
@@ -19,26 +19,32 @@ import jakarta.persistence.TemporalType;
 @Table(name = "transactions")
 public class Transaction {
 
+	// 계좌 거래 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "transaction_no")
     private Long transactionNo;
 
+    // 저축 계좌 객체
     @ManyToOne
     @JoinColumn(name = "account_serial_no", nullable = false)
     private SavingsAccount savingsAccount;
 
+    // 거래 날짜
     @Column(name = "transaction_date", nullable = false)
     @Temporal(TemporalType.DATE)
     private Date transactionDate;
 
+    // 거래 금액
     @Column(name = "amount", nullable = false)
     private Double amount;
 
+    // 거래 카테고리 객체
     @ManyToOne
     @JoinColumn(name = "category_no", nullable = false)
     private TransactionCategory category;
 
+    // 거래 설명
     @Column(name = "description", length = 255)
     private String description;
 
