92807b1f6631eec9fd3552132e04245343ac5727
이지희 <som2plestory@naver.com>
2024-08-16 06:56:20 +0900

    Update SavingsAccount.java

--- backend/src/main/java/com/eyes/solstory/domain/savings/entity/SavingsAccount.java
+++ backend/src/main/java/com/eyes/solstory/domain/savings/entity/SavingsAccount.java
@@ -19,42 +19,51 @@ import jakarta.persistence.TemporalType;
 @Table(name = "savings_accounts")
 public class SavingsAccount {
 
+	// 저축 계좌 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "account_serial_no")
     private Long accountSerialNo;
 
+    // 사용자 객체
     @ManyToOne
     @JoinColumn(name = "user_no", nullable = false)
     private User user;
 
+    // 저축 계좌번호
     @Column(name = "account_no", nullable = false, length = 34)
     private String accountNo;
 
+    // 계좌 별명
     @Column(name = "account_name", nullable = false, length = 100)
     private String accountName;
 
+    // 계좌 잔액
     @Column(name = "balance", nullable = false)
     private Long balance;
 
+    // 생성 일자
     @Temporal(TemporalType.DATE)
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
 
+    // 저축 회차
     @Column(name = "savings_round")
     private Integer savingsRound;
 
+    // 최근 입금 일자
     @Temporal(TemporalType.DATE)
     @Column(name = "last_deposit_date", nullable = false)
     private Date lastDepositDate;
 
+    // 계좌 상태(0 : 종료, 1:활성화)
     @Column(name = "status", nullable = false)
     private Integer status;
 
+    // 계좌 유형(1: 저축 계좌, 2: 주거래 계좌)
     @Column(name = "account_types", nullable = false)
     private Integer accountTypes;
 
-    // 기본 생성자
     public SavingsAccount() {}
 
     // Getters and Setters
