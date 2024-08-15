2b2ced091ce36dbca912e5b1fddb238bc9973ab2
이지희 <som2plestory@naver.com>
2024-08-16 06:56:17 +0900

    Update DepositHistory.java

--- backend/src/main/java/com/eyes/solstory/domain/savings/entity/DepositHistory.java
+++ backend/src/main/java/com/eyes/solstory/domain/savings/entity/DepositHistory.java
@@ -16,18 +16,23 @@ import jakarta.persistence.TemporalType;
 @Entity
 @Table(name = "deposit_history")
 public class DepositHistory {
+	
+	// 입금 내역 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "deposit_no")
     private Long depositNo;
 
+    // 저축 계좌 객체
     @ManyToOne
     @JoinColumn(name = "account_serial_no", nullable = false)
     private SavingsAccount savingsAccount;
 
+    // 저축 금액
     @Column(name = "deposit_amount", nullable = false)
     private Long depositAmount;
 
+    // 입금 일자
     @Temporal(TemporalType.DATE)
     @Column(name = "deposit_date", nullable = false)
     private Date depositDate;
