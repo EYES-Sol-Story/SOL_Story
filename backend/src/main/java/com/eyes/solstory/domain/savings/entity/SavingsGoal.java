f03ac65eebdaeb6a3f17e6f6b894fc61fd66a03d
이지희 <som2plestory@naver.com>
2024-08-16 06:56:22 +0900

    Update SavingsGoal.java

--- backend/src/main/java/com/eyes/solstory/domain/savings/entity/SavingsGoal.java
+++ backend/src/main/java/com/eyes/solstory/domain/savings/entity/SavingsGoal.java
@@ -17,35 +17,44 @@ import jakarta.persistence.TemporalType;
 @Table(name = "savings_goals")
 public class SavingsGoal {
 
+	// 저축 목표 일련번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "goal_no")
     private Long goalNo;
 
+    // 저축 계좌 객체
     @ManyToOne
     @JoinColumn(name = "account_serial_no", nullable = false)
-    private SavingsAccount relatedAccount; // SavingsAccount Relationship
+    private SavingsAccount relatedAccount; 
 
+    // 저축 목표 카테고리 객체
     @ManyToOne
     @JoinColumn(name = "category_no", nullable = false)
-    private SavingsGoalCategory category; // SavingsGoalCategory Relationship
+    private SavingsGoalCategory category; 
 
+    // 사용자 지정 목표
     @Column(name = "custom_goal", length = 100)
     private String customGoal;
 
+    // 저축 목표 금액
     @Column(name = "target_amount", nullable = false)
     private Long targetAmount;
 
+    // 현재 저축 금액
     @Column(name = "current_amount", nullable = false)
     private Long currentAmount;
 
+    // 매달 저축 산정 금액
     @Column(name = "monthly_deposit_amount", nullable = false)
     private Long monthlyDepositAmount;
 
+    // 시작일자
     @Temporal(TemporalType.DATE)
     @Column(name = "start_date", nullable = false)
     private Date startDate;
 
+    // 종료일자
     @Temporal(TemporalType.DATE)
     @Column(name = "end_date", nullable = false)
     private Date endDate;
