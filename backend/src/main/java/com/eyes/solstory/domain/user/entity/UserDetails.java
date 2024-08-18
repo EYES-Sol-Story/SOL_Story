3b098b14248568b54bee27cfd975b2c43917cb5e
som2 <som2plestory@naver.com>
2024-08-18 23:04:31 +0900

    Create UserDetails.java

---
+++ backend/src/main/java/com/eyes/solstory/domain/user/entity/UserDetails.java
@@ -0,0 +1,31 @@
+package com.eyes.solstory.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "User_details")
+public class UserDetails {	
+
+	@ManyToOne
+    @JoinColumn(name = "user_no", nullable = false)
+    private User user;
+
+    @Column(name = "attribute_type", nullable = false)
+    private Integer attributeType;
+
+    @Column(name = "attribute_value", nullable = false, length = 50)
+    private String attributeValue;
+
+}
