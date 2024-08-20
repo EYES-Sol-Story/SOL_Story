39aae6f550d4d6df785c6c82e8824a16b777d0c2
SOM2 <som2plestory@naver.com>
2024-08-20 17:55:56 +0900

    create api util class

--- backend/src/main/java/com/eyes/solstory/constants/OpenApiUrls.java
+++ backend/src/main/java/com/eyes/solstory/constants/OpenApiUrls.java
@@ -1,26 +1,29 @@
 package com.eyes.solstory.constants;
 
 public class OpenApiUrls {
-	//// URL 공통
-	static final String BASE_URL = "https//finopenapi.ssafy.io/ssafy/api/v1/edu";
+	//// 입출금 URL 공통 
+	public static final String DEMAND_DEPOSIT_URL = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit/";
+	//// 적금 URL 공통 
+	public static final String SAVINGS_URL = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/savings/";
+	
 	
 	//// 입출금 계좌
 	// 예금주 조회
-	static final String INQUIRE_DEMAND_DEPOSIT_ACCOUNT_HOLDER_NAME = "/demandDeposit/inquireDemandDepositAccountHolderName";
+	public static final String INQUIRE_DEMAND_DEPOSIT_ACCOUNT_HOLDER_NAME = "inquireDemandDepositAccountHolderName";
 	// 계좌 잔액 조회
-	static final String INQUIRE_DEMAND_DEPOSIT_ACCOUNT_BALANCE = "/demandDeposit/inquireDemandDepositAccountBalance";
+	public static final String INQUIRE_DEMAND_DEPOSIT_ACCOUNT_BALANCE = "inquireDemandDepositAccountBalance";
 	// 계좌 출금
-	static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_WITHDRAWAL = "/demandDeposit/updateDemandDepositAccountWithdrawal";
+	public static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_WITHDRAWAL = "updateDemandDepositAccountWithdrawal";
 	// 계좌 입금
-	static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_DEPOSIT = "/demandDeposit/updateDemandDepositAccountDeposit";
+	public static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_DEPOSIT = "updateDemandDepositAccountDeposit";
 	// 계좌 이체
-	static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_TRANSFER = "/demandDeposit/updateDemandDepositAccountTransfer";
+	public static final String UPDATE_DEMAND_DEPOSIT_ACCOUNT_TRANSFER = "updateDemandDepositAccountTransfer";
 	// 계좌 거래 내역 조회
-	static final String INQUIRE_TRANSACTION_HISTORY_LIST = "/demandDeposit/inquireTransactionHistoryList";
+	public static final String INQUIRE_TRANSACTION_HISTORY_LIST = "inquireTransactionHistoryList";
 	// 계좌 거래 내역 조회(단건)
-	static final String INQUIRE_TRANSACTION_HISTORY = "/demandDeposit/inquireTransactionHistory";
+	public static final String INQUIRE_TRANSACTION_HISTORY = "inquireTransactionHistory";
 
 	//// 적금 계좌
 	// 적금 납입 회차 조회
-	static final String INQUIRE_PAYMENT = "/savings/inquirePayment";
+	public static final String INQUIRE_PAYMENT = "inquirePayment";
 }
