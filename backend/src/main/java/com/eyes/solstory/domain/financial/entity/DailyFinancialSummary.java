36ebffefd87e439b7c8049e689c75f3f3886017a
SOM2 <som2plestory@naver.com>
2024-08-21 10:36:26 +0900

    기존 브랜치 삭제 후 추가

--- backend/src/main/java/com/eyes/solstory/domain/financial/entity/DailyFinancialSummary.java
+++ backend/src/main/java/com/eyes/solstory/domain/financial/entity/DailyFinancialSummary.java
@@ -2,46 +2,49 @@ package com.eyes.solstory.domain.financial.entity;
 
 import java.time.LocalDate;
 
-import com.eyes.solstory.domain.user.entity.User;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Entity
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "daily_financial_summary")
+@SequenceGenerator(
+	    name = "summary_seq_generator",
+	    sequenceName = "summary_seq", // 오라클에 생성한 시퀀스 이름
+	    allocationSize = 1  // 시퀀스 값을 하나씩 증가
+	)
 public class DailyFinancialSummary {
 
 	// summary 일련번호
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "summary_seq_generator")
     @Column(name = "summary_no", precision = 10)
     private int summaryNo;
 
     // 사용자 일련번호
-    @ManyToOne
-    @JoinColumn(name = "user_no", nullable = false)
-    private User user;
+    @Column(name = "user_no", nullable = false)
+    private int userNo;
 
-    // 저축/소비 해당 날짜
+    // 지출 날짜 
     @Column(name = "financial_date", nullable = false)
     private LocalDate financialDate;
 
     // 산출 유형(1: 저축 , 2:소비)
-    @Column(name = "financial_type", precision = 1, nullable = false)
+    @Column(name = "financial_type", nullable = false, precision = 1)
     private int financialType;
 
     // 지출 카테고리
@@ -49,6 +52,6 @@ public class DailyFinancialSummary {
     private String category;
 
     // 총액(저축/지출 카테고리별)
-    @Column(name = "total_amount", precision = 1, nullable = false)
+    @Column(name = "total_amount", nullable = false)
     private int totalAmount;
 }
