1261126a9334bfb381f66f60d5ddb72ca71cdbec
som2 <som2plestory@naver.com>
2024-08-24 11:45:50 +0900

    Update UserAccountService.java

--- backend/src/main/java/com/eyes/solstory/domain/financial/service/UserAccountService.java
+++ backend/src/main/java/com/eyes/solstory/domain/financial/service/UserAccountService.java
@@ -2,13 +2,12 @@ package com.eyes.solstory.domain.financial.service;
 
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.eyes.solstory.domain.financial.dto.ActiveAccountDTO;
 import com.eyes.solstory.domain.financial.repository.UserAccountRepository;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
