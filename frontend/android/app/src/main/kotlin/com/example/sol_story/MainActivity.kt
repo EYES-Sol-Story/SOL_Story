3ecf7ad5ca12938a47422ea0dddcc2eefa011a27
SOM2 <133189092+SOM2-RECORD@users.noreply.github.com>
2024-08-31 16:43:39 +0900

    Delete frontend directory

--- frontend/android/app/src/main/kotlin/com/example/sol_story/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sol.sol_story
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- frontend/lib/config/constants.dart
+++
@@ -1,4 +0,0 @@
-const REST_API_URL = 'http://10.0.2.2:8090';
-
-//지히 - 테스트용
-const String email = "240827_02@ssafy.com";
